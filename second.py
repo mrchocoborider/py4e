bb8bd53b216c77960e0e9581cd6db0d7f42ec307
mrchocoborider <mrchocoborider@gmail.com>
2019-05-09 19:19:21 -0700

    Finished chapter 10 exercises, started chapter 11

--- second.py
+++ second.py
@@ -19,7 +19,24 @@ def romeo(fname):
             else: t.append(i)
     t.sort()
     print(t)
-                
+
+
+#This is a modified version of the code from 10.6
+def romeo2(fname):
+    fhand = open(fname)
+    count = {}
+    t = []
+    for line in fhand:
+        words = line.split()
+        if len(words) == 0: continue
+        for word in words:
+            count[word] = count.get(word, 0) + 1
+    for key, val in count.items():
+        t.append((val, key))
+    t.sort(reverse=True)
+    print(t)
+
+
 def frmfinder(fname):
     fhand = open(fname)
     count = 0
@@ -42,6 +59,8 @@ def frmfinder2(fname):
         dowd[words[2]] = dowd.get(words[2], 0) + 1
     print(dowd)
 
+
+#Modified for Chapter 10 Exercise 1
 def maillog(fname):
     fhand = open(fname)
     md = {}
@@ -52,14 +71,46 @@ def maillog(fname):
         md[words[1]] = md.get(words[1], 0) + 1
     print(md)
 
+    t = []
+    for key, val in md.items():
+        t.append((val, key))
+    t.sort(reverse=True)
+    print(t[0])
+
     #Exercise 4 chapter 9
-    largest = 0
-    mail = ''
-    for x in md:
-        if md[x] > largest:
-            largest = md[x]
-            mail = x
-    print(mail, largest)
+    #largest = 0
+    #mail = ''
+    #for x in md:
+    #    if md[x] > largest:
+    #        largest = md[x]
+    #        mail = x
+    #print(mail, largest)
+
+#Exercise 2 chapter 10
+def timeofday(fname):
+    fhand = open(fname)
+    md = {}
+    for line in fhand:
+        words = line.split()
+        if len(words) == 0: continue
+        if words[0] != 'From': continue
+        tod = words[5]
+        tods = tod.split(':')
+        #get the hours
+        hr = tods[0]
+        md[hr] = md.get(hr, 0) + 1
+    #Whoops, sorted by count, exercise says sort by hour
+    #t = []
+    #for key, val in md.items():
+    #    t.append((val, key))
+    #t.sort(reverse=True)
+    #for h in t:
+    #    print(h[1], h[0])
+    z = list(md.items())
+    z.sort()
+    for y in z:
+        print(y[0], y[1])
+
 
 #Exercise 5
 def schoolcount(fname):
@@ -107,8 +158,13 @@ if __name__ == '__main__':
         elif arg.lower() == 'schoolcount':
             fname = 'mbox-short.txt'
             schoolcount(fname)
+        elif arg.lower() == 'romeo2':
+            fname = 'romeo.txt'
+            romeo2(fname)
+        elif arg.lower() == 'time':
+            fname = 'mbox-short.txt'
+            timeofday(fname)
 
-        #elif arg.lower() == 'spamconf':
         else:
             print('I do not recognize that command: ')
 
